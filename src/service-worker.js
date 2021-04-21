c8ae2e17aa90969054d7ceec08abd51fb8b22fba
aswinzz <aswinvb.aswin6@gmail.com>
2021-04-21 20:10:39 +0530

    updates

--- src/service-worker.js
+++ src/service-worker.js
@@ -69,6 +69,28 @@ self.addEventListener('message', (event) => {
   }
 });
 
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
+  'index.html',
+  'offline.html'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+
 self.addEventListener('fetch', event => {
   console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(caches.match(event.request)
@@ -77,6 +99,12 @@ self.addEventListener('fetch', event => {
           return cachedResponse;
         }
         return fetch(event.request);
+      }).catch(function() {
+        // If both fail, show a generic fallback:
+        return caches.match('/offline.html');
+        // However, in reality you'd have many different
+        // fallbacks, depending on URL & headers.
+        // Eg, a fallback silhouette image for avatars.
       })
     );
 });
